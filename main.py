e1f4d49f6ec184e3b5f28d8e6be407b7c07c0882
Katherine Zhang <156546096+zhkat@users.noreply.github.com>
2024-09-21 22:15:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ def main():
     citywide_disparity = DataReader(citywide_disparity_filename)
     health_of_city = DataReader(health_of_city_filename)
 
-    data_processor = DataProcessor(health_centers, population_metrics_census_tract, population_metrics_citywide, citywide_disparity, health_of_city)
+    data_processor = None
+    try:
+        data_processor = DataProcessor(health_centers, population_metrics_census_tract, population_metrics_citywide, citywide_disparity, health_of_city)
+    except:
+        print('Error: One or more of the provided files cannot be opened or read')
 
     #code to call method(s) to process data go here
 
